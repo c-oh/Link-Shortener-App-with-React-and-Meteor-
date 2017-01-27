a5dc2109ce6f318ad166f231ed97dbb27f4bc6ef
hollythedev <papiniu@outlook.com>
2017-01-27 10:12:09 -0800

    imported links method to server and client side

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import Header from './components/header';
 import LinkCreate from './components/link_create';
+import {Links} from '../imports/collections/links';
+
 const App = () => {
     return (
         <div>
