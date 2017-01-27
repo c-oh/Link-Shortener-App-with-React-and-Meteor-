c1ca20064b4b420b62abc105b492213d2b61fd19
hollythedev <papiniu@outlook.com>
2017-01-27 09:49:11 -0800

    Prevents browser from refreshing once submit button is clicked

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 
 class LinkCreate extends Component {
+    handleSubmit(event){
+event.preventDefault();
+console.log(this.refs.link.value);
+    }
     render(){
         return(
-            <form>
+            <form onSubmit={this.handleSubmit.bind(this)}>
             <div className="form-group">
                 <label> Please insert the link that needs to be shortened! </label>
-                <input className ="form-control"/>
+                <input ref= "input" className ="form-control"/>
                 </div>
                 <button className= "btn btn-primary"> Shorten It! </button>
             </form>
