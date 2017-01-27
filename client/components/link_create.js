56184ede4c9731226ca241106be650a539404f6a
hollythedev <papiniu@outlook.com>
2017-01-26 23:24:13 -0800

    Added label, input and button to form

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -4,6 +4,11 @@ class LinkCreate extends Component {
     render(){
         return(
             <form>
+            <div className="form-group">
+                <label> Please insert the link that needs to be shortened! </label>
+                <input className ="form-control"/>
+                </div>
+                <button className= "btn btn-primary"> Shorten It! </button>
             </form>
         )
     }
--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/header';
-import LinkCreate from './components/links_create';
+import LinkCreate from './components/link_create';
 const App = () => {
     return (
         <div>
